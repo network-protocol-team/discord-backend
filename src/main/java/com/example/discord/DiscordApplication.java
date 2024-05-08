8c7c979a4e02afcd1f38cce1fceafdf184f28ff8
teria314 <bacteria3141592@gmail.com>
2024-05-08 15:29:35 +0900

    setting

---
+++ src/main/java/com/example/discord/DiscordApplication.java
@@ -0,0 +1,13 @@
+package com.example.discord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/discord/DiscordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.discord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
