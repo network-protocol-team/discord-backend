40265226949ed02565c1e30d579627fea55a6ef5
sojungoh <sojung3514@gmail.com>
2024-05-14 16:05:59 +0900

    fix: test code 수정

--- src/test/java/com/example/discord/service/UserServiceIntegrationTest.java
+++ src/test/java/com/example/discord/service/UserServiceIntegrationTest.java
@@ -1,5 +1,6 @@
 package com.example.discord.service;
 
+import com.example.discord.common.exception.BaseException;
 import com.example.discord.common.response.BaseResponse;
 import com.example.discord.src.controller.UserController;
 import com.example.discord.src.dto.PostUserReq;
@@ -7,11 +8,13 @@ import com.example.discord.src.entity.User;
 import com.example.discord.src.repository.UserRepository;
 import com.example.discord.src.service.UserService;
 import com.example.discord.src.dto.PostUserRes;
-import jakarta.servlet.http.HttpServletRequest;
+import jakarta.transaction.Transactional;
+import jakarta.validation.constraints.Null;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cache.support.NullValue;
 
 import java.util.Optional;
 
@@ -27,41 +30,46 @@ public class UserServiceIntegrationTest {
     @Autowired UserRepository userRepository;
 
     @Test
+    @Transactional
     public void signUp() throws Exception {
         String nickName = "오소정";
         PostUserReq postUserReq = new PostUserReq();
         postUserReq.setNickName(nickName);
 
-        Optional<PostUserRes> postUserRes = userService.signUp(postUserReq);
-
-        if(postUserRes.isPresent()) {
-            log.info("userId: {}", postUserRes.get().getUserId());
-            //log.info("cookie: {}", postUserRes.get().getCookie());
+        try {
+            PostUserRes postUserRes = userService.signUp(postUserReq);
+            log.info("userId: {}", postUserRes.getUserId());
 
             User user = userRepository.findByNickName(nickName).get();
             assertEquals(user.getNickName(), nickName);
-        }
-        else {
+
+        }catch(BaseException baseException) {
+            log.info("status: {}", baseException.getStatus());
+            log.info("message: {}", baseException.getMessage());
+
             Optional<User> user = userRepository.findByNickName(nickName);
             assertThat(user.isPresent());
         }
     }
 
     @Test
+    @Transactional
     public void signUpFromController() throws Exception {
         PostUserReq postUserReq = new PostUserReq();
-        postUserReq.setNickName("user2");
+        postUserReq.setNickName("user6");
 
         BaseResponse responseBody = userController.signUp(postUserReq);
 
         log.info("suceess: {}", responseBody.getIsSuccess());
         log.info("code: {}", responseBody.getCode());
         log.info("message: {}", responseBody.getMessage());
-        if(responseBody.getIsSuccess()) {
-            PostUserRes postUserRes = (PostUserRes) responseBody.getResult();
+
+        Optional postUserRes = Optional.ofNullable(responseBody.getResult());
+
+        if(postUserRes.isPresent()) {
             log.info("result: ");
-            log.info("nickName: {}", postUserRes.getUserId());
-            log.info("cookie: {}", postUserRes.getCookie().getValue());
+            log.info("nickName: {}", ((PostUserRes) postUserRes.get()).getNickName());
+            log.info("cookie: {}", ((PostUserRes) postUserRes.get()).getCookie().getValue());
         }
     }
 }
