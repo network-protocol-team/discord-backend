e4e557a75279666764b0bd8fbe4a754f7c048c2b
sojungoh <sojung3514@gmail.com>
2024-05-15 16:23:49 +0900

    fix: userService test code 수정

--- src/test/java/com/example/discord/service/UserServiceIntegrationTest.java
+++ src/test/java/com/example/discord/service/UserServiceIntegrationTest.java
@@ -9,18 +9,15 @@ import com.example.discord.src.repository.UserRepository;
 import com.example.discord.src.service.UserService;
 import com.example.discord.src.dto.PostUserRes;
 import jakarta.transaction.Transactional;
-import jakarta.validation.constraints.Null;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cache.support.NullValue;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @Slf4j
 @SpringBootTest
@@ -31,10 +28,9 @@ public class UserServiceIntegrationTest {
 
     @Test
     @Transactional
-    public void signUp() throws Exception {
+    public void signUp() {
         String nickName = "오소정";
-        PostUserReq postUserReq = new PostUserReq();
-        postUserReq.setNickName(nickName);
+        PostUserReq postUserReq = new PostUserReq(nickName);
 
         try {
             PostUserRes postUserRes = userService.signUp(postUserReq);
@@ -53,23 +49,21 @@ public class UserServiceIntegrationTest {
     }
 
     @Test
-    @Transactional
-    public void signUpFromController() throws Exception {
-        PostUserReq postUserReq = new PostUserReq();
-        postUserReq.setNickName("user6");
+    public void signUpFromController() {
+        PostUserReq postUserReq = new PostUserReq("user7");
 
-        BaseResponse responseBody = userController.signUp(postUserReq);
+        BaseResponse<PostUserRes> responseBody = userController.signUp(postUserReq);
 
-        log.info("suceess: {}", responseBody.getIsSuccess());
+        log.info("success: {}", responseBody.getIsSuccess());
         log.info("code: {}", responseBody.getCode());
         log.info("message: {}", responseBody.getMessage());
 
-        Optional postUserRes = Optional.ofNullable(responseBody.getResult());
+        Optional<PostUserRes> postUserRes = Optional.ofNullable(responseBody.getResult());
 
         if(postUserRes.isPresent()) {
             log.info("result: ");
-            log.info("nickName: {}", ((PostUserRes) postUserRes.get()).getNickName());
-            log.info("cookie: {}", ((PostUserRes) postUserRes.get()).getCookie().getValue());
+            log.info("nickName: {}", postUserRes.get().getNickName());
+            log.info("cookie: {}", postUserRes.get().getCookie().getValue());
         }
     }
 }
